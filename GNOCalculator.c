8501232959ee48aa249bff7a5a306b9c79ee2dba
Sefa <mail@sefakalkan.com>
2020-03-18 20:30:59 +0300

    Formatted and beautiefied

--- GNOCalculator.c
+++ GNOCalculator.c
@@ -1,37 +1,32 @@
 #include<stdio.h>
 
-int  main(){
+int main() {
 
+        float GANO = 0.00;
+        float GNO = 0.00;
+        
+        int numoflessons;
+        int akts[999];
+        int i;
+        int totalcredit = 0;
+        
+        float points[999];
+        
+        printf("Enter the Total Number of lessons \n ");
+        scanf("%d", & numoflessons);
 
-float GANO = 0.00;
-float GNO = 0.00;
-int numoflessons;
-int akts[999];
-float points[999];
-int i;
-int totalcredit = 0;
+        for (i = 0; i < numoflessons; i++) {
 
-printf("Enter the Total Number of lessons \n ");
+                printf("\n Enter the Credit and Point of lesson %d \n", i + 1);
+                
+                scanf("%d%f", & akts[i], & points[i]);
+                
+                totalcredit += akts[i];
+                GANO += akts[i] * points[i];
 
-scanf("%d", &numoflessons);
-
-for(i=0;i<numoflessons;i++){
-
-printf("\n Enter the Credit and Point of lesson %d \n", i+1);
-
-scanf("%d%f", &akts[i], &points[i]);
-
-totalcredit += akts[i];
-
-GANO += akts[i] * points[i] ;
-
-}
-
-GNO = GANO/totalcredit;
-
-printf("%F",GNO);
-
-
-
-    return 0;
+        }
+        GNO = GANO / totalcredit;
+        printf("%F", GNO);
+        
+        return 0;
 }
