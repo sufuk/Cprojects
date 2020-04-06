28b149b5221a5791053a20f6531f65eaddf9f14e
Semih Ufuk Güler <sufukguler@gmail.com>
2020-04-06 18:07:26 +0300

    My string libary
    Added example for my_strncat

--- mystinglibary.c
+++ mystinglibary.c
@@ -30,5 +30,9 @@ char stra[] = "sufuk";
 char strb[] = "erdem";
 my_strcat(stra, strb);
 printf("\n %s", stra);
+char strc[] = "sufuk";
+char strd[] = "erdem";
+my_strncat(strc, strd, 3);
+printf("\n%s", strc);
   return 0;
 }
