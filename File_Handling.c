acfb9d760c412ea74983cd76578285eac64756b7
Semih Ufuk Güler <sufukguler@gmail.com>
2020-04-14 19:23:35 +0300

    Create File_Handling.c

---
+++ File_Handling.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+int main(){
+
+FILE* spData;
+
+//how to open a file to write it
+//how to open a file to read from it
+// how to open a file to append data to it
+
+spData = fopen("README.md", "r");
+
+
+
+
+    return 0;
+}
