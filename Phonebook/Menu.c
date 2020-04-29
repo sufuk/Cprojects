1d7a134dd31e798cb23128cd6968122cd6599c15
sufuk <sufukguler@gmail.com>
2020-04-29 15:03:14 +0300

    Phonebook app
    Main application source code

---
+++ Phonebook/Menu.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+int main(){
+
+
+printf("\nMENU\n");
+
+printf("[1] List Contacts\n");
+printf("[2] Search a Contact\n");
+printf("[3] Add a Contact\n");
+printf("[4] Remove a Contact\n");
+
+int a;
+scanf("%d", &a);
+
+if(a == 1){
+
+//List contcacts
+
+
+
+
+
+
+
+}
+//////////////
+
+if(a == 2){
+
+
+}
+//////////////////////7
+
+if(a == 3){
+
+
+}
+////////////////777
+
+if(a == 4){
+
+
+
+}
+
+///////////////////7777777
+
+
+
+
+
+
+
+
+  return 0;
+}
