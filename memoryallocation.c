e9acc3bcf0e43c12247999b9a679bfd11456cc45
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-02 22:14:42 +0300

    Update memoryallocation.c

--- memoryallocation.c
+++ memoryallocation.c
@@ -1,39 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+// You can also find in my github repository:
+// https://github.com/sufuk/Cprojects
 int main(){
-
 int *p;
 int i;
 int a;
 int sum = 0;
 printf("Please enter the number of elements\n");
 scanf("%d", &a);
-
 p = (int*)malloc(a * sizeof(int));
-
 for(i = 0; i < a ; i++){
   printf("Please enter the number [%d]\n", i+1);
   scanf("%d", &p[i]);
   sum += p[i];
 }
-
-
 printf("Sum of elements are :%d", sum);
-
-
-
-
-
 free(p);
-
-
-
-
-
-
-
-
-
-
   return 0;
 }
