da36e617990f2d0c636e9e8aef5528e07202117f
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-05 10:24:48 +0300

    Update Hangman.c

--- Hangman.c
+++ Hangman.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main(){
 
 char man[] = "Sufuk" ;
-int i = 0;
+int i;
 
 printf("Welcome to hangman\n");
 
 for(i ; i <=5 ; ){
   printf("Please enter your guess\n");
-  scanf("%c", a);
+  scanf("%c", &a);
 
-  if
-
+    if(a == )
 
 
 
