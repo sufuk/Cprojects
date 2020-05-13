1bbc627c548bf4cb572fe763c9fd8ecf311ab24d
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-13 21:03:02 +0300

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -15,6 +15,12 @@ void printlist(struct Node *n){
     n = n->next;
   }
 }
+
+void addelement(struct Node *n, int element){
+  
+
+
+}
 int main(){
   struct Node* head = NULL;
   struct Node* second = NULL;
@@ -30,22 +36,6 @@ int main(){
   third->data = 3;
   third->next = NULL;
   printlist(head);
-
-  struct Node* head1 = NULL;
-  struct Node* second1 = NULL;
-  struct Node* third1 = NULL;
-
-  head1 = (struct Node*)malloc(sizeof(struct Node));
-  second1 = (struct Node*)malloc(sizeof(struct Node));
-  third1 = (struct Node*)malloc(sizeof(struct Node));
-
-  head1->data = 4;
-  head1->next = second1;
-  second1->data = 5;
-  second1->next = third1;
-  third1->data = 6;
-  third1->next = head;
-  printlist(head1);
   return 0;
 
 
